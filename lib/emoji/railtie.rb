7df40db14b85c14ce967616f9810c161498b576c
Glenn Sidney <20549+glennfu@users.noreply.github.com>
2023-05-06 15:52:57 -0400

    Only use sprockets if available

--- lib/emoji/railtie.rb
+++ lib/emoji/railtie.rb
@@ -1,13 +1,21 @@
 require 'emoji'
 require 'rails'
-require 'sprockets'
+
+begin
+  require 'sprockets'
+  sprockets_available = true
+rescue LoadError
+  sprockets_available = false
+end
 
 module Emoji
   class Railtie < Rails::Railtie
     initializer "emoji.defaults" do
       Emoji.asset_host = ActionController::Base.asset_host
-      asset_prefix = Rails.application.config.assets.prefix rescue '/assets'
-      Emoji.asset_path =  File.join(asset_prefix, '/emoji')
+      if sprockets_available
+        asset_prefix = Rails.application.config.assets.prefix rescue '/assets'
+        Emoji.asset_path = File.join(asset_prefix, '/emoji')
+      end
       Emoji.use_plaintext_alt_tags = false
     end
 
