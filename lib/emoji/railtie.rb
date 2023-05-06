eb04aec91e744d0f1ef573fedcc944e9341e1f1e
Glenn Sidney <20549+glennfu@users.noreply.github.com>
2023-05-06 16:13:49 -0400

    Try another way to detect sprockets

--- lib/emoji/railtie.rb
+++ lib/emoji/railtie.rb
@@ -1,21 +1,16 @@
 require 'emoji'
 require 'rails'
 
-begin
-  require 'sprockets'
-  sprockets_available = true
-rescue LoadError
-  sprockets_available = false
-end
-
 module Emoji
   class Railtie < Rails::Railtie
+    def sprockets_available?
+      defined?(Rails.application.config.assets) && Rails.application.config.assets.is_a?(Sprockets::Railtie::Configuration)
+    end
+
     initializer "emoji.defaults" do
       Emoji.asset_host = ActionController::Base.asset_host
-      if sprockets_available
-        asset_prefix = Rails.application.config.assets.prefix rescue '/assets'
-        Emoji.asset_path = File.join(asset_prefix, '/emoji')
-      end
+      asset_prefix = sprockets_available? ? Rails.application.config.assets.prefix : '/assets'
+      Emoji.asset_path = File.join(asset_prefix, '/emoji')
       Emoji.use_plaintext_alt_tags = false
     end
 
